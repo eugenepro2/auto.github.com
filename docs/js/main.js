18c14c062feb857fbb9ae16d5421b6e18b373d21
kabyr <eugenepro@icloud.com>
2017-10-03 23:02:27 +0300

    mobile

--- docs/js/main.js
+++ docs/js/main.js
@@ -23,15 +23,28 @@ var swiper = new Swiper('#review', {
     spaceBetween: 30,
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
+    breakpoints: {
+      1024: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      }}
 });
 
+  //Мобильная менюшка
 var slideout = new Slideout({
-    'panel': document.getElementById('panel'),
-    'menu': document.getElementById('menu'),
-    'padding': 256,
-    'tolerance': 70
-  });
-
+  'panel': document.getElementById('panel'),
+  'menu': document.getElementById('menu'),
+  'padding': 256,
+  'tolerance': 70,
+});
 document.querySelector('.menu-open').addEventListener('click', function() {
   slideout.toggle();
+});
+
+slideout.on('open', function() { 
+  $('#btn').addClass('on');
+});
+
+slideout.on('close', function() { 
+  $('#btn').removeClass('on');
 });
